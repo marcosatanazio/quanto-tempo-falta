05066d3d543ae8b8c46f9ec54c2ae00ada42ae18
marcosatanazio <107881237+marcosatanazio@users.noreply.github.com>
2024-05-07 12:26:05 -0300

    a

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
  const dataAtatua = new Date ();
- let data
+ let dataObjetivo = prompt (¨Data final ANO-MES_DIA Exemplo:2024-04-10¨);
+
+ dataObjetivo = new Date (dataObjetivo+ ¨t23:59:59);
+ let diasQueFaltam = math.floor ((dataObjetivo-dataAtual)/ 86400000);
+  
+ document.querySelector(¨#dias_restantes¨).textContent = diasQueFaltam;
